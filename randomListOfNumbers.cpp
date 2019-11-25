28a1f3d104c2f1e6520125d290dafb4c2a7672d9
TungKhanhTran <tung.tran.1805@hcmut.edu.vn>
2019-11-25 13:05:06 +0700

    Update the cpp code that random 20 lists of numbers

--- randomListOfNumbers.cpp
+++ randomListOfNumbers.cpp
@@ -5,7 +5,7 @@ int LISTSIZE = 50;
 
 int main()  {
     
-    for (int j = 0; j < 19; j++)    {
+    for (int j = 0; j < 20; j++)    {
         for (int i = 0; i < LISTSIZE - 1; i++)    {
             cout << rand() % 131072 - 65536 << ", ";
         }
